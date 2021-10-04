6bdeb64572ab8fd4426e87dadb4ada9d65a51b78
Shakil Hossain <shakilhossainp.cse@gmail.com>
2021-10-04 12:31:11 +0600

    added more services

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,17 +1,21 @@
-import React from 'react'
+import {React,useState,useEffect} from 'react'
 import {  Container, Row } from 'react-bootstrap'
-import { useServices } from '../../hook/useServices'
 import { Course } from '../Course/Course'
 
 export const Services = () => {
-   const [courses] = useServices()
+    const [services, setServices] = useState([])
+    useEffect(()=>{
+        fetch('./services.json')
+        .then(res => res.json())
+        .then(data => setServices(data))
+      },[])
     return (
         <div className="bg-light">
             <Container className="py-5">
                 <h1 className="py-4">All Services </h1>
                 <Row xs={1} md={2} className="g-5">
                     {
-                        courses.map(course => <Course course={course}/>)
+                        services.map(service => <Course course={service}/>)
                     }
                 </Row>
             </Container>
