f5ec8b6301fce8c493ca4f18c22eae80b6c65eed
Shakil Hossain <shakilhossainp.cse@gmail.com>
2021-10-04 12:51:59 +0600

    responsive header section

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,25 @@
 import React from 'react'
-import { Container, Navbar } from 'react-bootstrap'
+import { Container, Nav, Navbar } from 'react-bootstrap'
 import { NavLink } from 'react-router-dom'
 import './Header.css'
 
 export const Header = () => {
     return (
-        <Navbar>
-        <Container className="py-2">
-          <Navbar.Brand className="fw-bold fs-1 border border-5 border-primary"><NavLink className="text-decoration-none" to="/">NP</NavLink></Navbar.Brand>
-          <Navbar.Toggle />
-          <Navbar.Collapse className="justify-content-end">
-            <Navbar.Text className="d-flex">
-                <NavLink  className="text-decoration-none mx-4 text-uppercase nav-link" to="/">Home</NavLink>
-                <NavLink className="text-decoration-none mx-4 text-uppercase nav-link" to="/services">Services</NavLink>
-                <NavLink className="text-decoration-none mx-4 text-uppercase nav-link" to="/about">About Us</NavLink>
-                <NavLink className="text-decoration-none mx-4 text-uppercase nav-link" to="/signup">Sing Up</NavLink>
-            </Navbar.Text>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+      <Navbar bg="light" expand="lg">
+      <Container>
+      <Navbar.Brand className="fw-bold fs-1 border border-5 border-primary"><NavLink className="text-decoration-none" to="/">NP</NavLink></Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+          <NavLink  className="text-decoration-none mx-4 text-uppercase nav-link" to="/">Home</NavLink>
+                    <NavLink className="text-decoration-none mx-4 text-uppercase nav-link" to="/services">Services</NavLink>
+                    <NavLink className="text-decoration-none mx-4 text-uppercase nav-link" to="/about">About Us</NavLink>
+                    <NavLink className="text-decoration-none mx-4 text-uppercase nav-link" to="/signup">Sing Up</NavLink>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    
     )
 }
+
